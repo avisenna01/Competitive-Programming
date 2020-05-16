a55a647fe363e389cc4d10d701fef18c4d46660e
Avisenna Abimanyu <theknightmare07@gmail.com>
2020-05-16 15:04:50 +0700

    orac and factors updates

--- Avisenna/Orac and Factors.cpp
+++ Avisenna/Orac and Factors.cpp
@@ -5,6 +5,74 @@ using namespace std;
 
 int t;
 
+int isPrime(int n)
+{
+    int sr = pow(n, 0.5); // sqrt() can be used too
+    for (int i = 2; i <= sr; i++)
+    {
+        if (n % i == 0)
+            return i;
+    }
+
+    return 1;
+}
+
+int op(int n, int k)
+{
+    // for (int i = 0; i < k; i++)
+    // {
+    //     if (n % 2 == 0)
+    //     {
+    //         n = n + 2;
+    //     }
+    //     else if (isPrime(n))
+    //     {
+    //         n *= 2;
+    //     }
+    //     else // odd number
+    //     {    // find the smallest factor in odd number
+    //     }
+    // }
+    // return n;
+
+    if (n % 2 == 0)
+    {
+        n += 2 * k;
+    }
+    else if (isPrime(n) == 1)
+    {
+        n = n * 2 + 2 * (k - 1);
+    }
+    else
+    {
+        n = isPrime(n) + 2 * (k - 1);
+    }
+
+    return n;
+}
+
+// int lf(int n)
+// {
+//     for (int i = 2; i <= n; i++)
+//     {
+//         if (n % i == 0)
+//             return i;
+//     }
+// }
+
+// int op1(int n, int k)
+// {
+//     for (int i = 0; i < k; i++)
+//     {
+//         if (n >= 1000000)
+//             break;
+
+//         if (n + lf(n) <= 1000000)
+//             n += lf(n);
+//     }
+//     return n;
+// }
+
 int main()
 {
 
@@ -13,9 +81,11 @@ int main()
 
     cin >> t;
     int n, k;
+
     for (int i = 0; i < t; i++)
     {
         cin >> n >> k;
+        cout << op(n, k) << endl;
     }
 
     return 0;
