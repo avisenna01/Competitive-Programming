c4b69135dc4d1e755d0fe357656c37212688e913
Avisenna Abimanyu <theknightmare07@gmail.com>
2020-05-16 14:07:21 +0700

    a+b is finished

---
+++ Avisenna/Orac and Factors.cpp
@@ -0,0 +1,22 @@
+//https://codeforces.com/problemset/problem/1350/A
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int t;
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> t;
+    int n, k;
+    for (int i = 0; i < t; i++)
+    {
+        cin >> n >> k;
+    }
+
+    return 0;
+}
